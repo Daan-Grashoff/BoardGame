d8ffc118996f3d29c4064d0e10fafd428fd623cb
Stefan Koolen <nast3zz@gmail.com>
2016-01-18 19:02:45 +0100

    Removed love.mousepressed
    Moved it to main.lua

--- game.lua
+++ game.lua
@@ -20,26 +20,6 @@ function game:keypressed(key, gameState)
   end
 end
 
-function love.mousepressed(x, y, button)
-	for _,t in pairs(board.tiles) do
-		if x > t.x and x < t.x + t.width
-		and y > t.y and y < t.y + t.height
-		and t.atributes.tank
-		then
-			print('menu')
-		end
-	end
-	for _,object in pairs(objects.items) do
-		if x > object.x and x < object.x + object.width
-		and y > object.y and y < object.y + object.height
-		then
-			object.dragging.active = true
-			object.dragging.diffX = x - object.x
-			object.dragging.diffY = y - object.y
-		end
-	end
-
-end
 
 function game:draw()
 	love.graphics.print("GAME", 200, 100)
