aa506163ca699046b04b26bae3fd34aacaa78fa7
Stefan Koolen <nast3zz@gmail.com>
2016-01-18 19:03:50 +0100

    Removed setTitle, added love.mousepressed
    Removed love.window.setTitle because it's currently not working. And
    moved the mousepressed function that was comming from game.lua

--- main.lua
+++ main.lua
@@ -4,7 +4,6 @@ function love.load()
 
 	width = 1080
 	height = 763
-	love.window.setTitle = 'Board Game'
 
 	love.window.setMode(width, height)
 
@@ -25,10 +24,31 @@ function love:keypressed(key)
   end
 end
 
+function love.mousepressed(x, y, button)
+  for _,t in pairs(board.tiles) do
+    if x > t.x and x < t.x + t.width
+    and y > t.y and y < t.y + t.height
+    and t.atributes.tank
+    then
+      print('menu')
+    end
+  end
+  for _,object in pairs(objects.items) do
+    if x > object.x and x < object.x + object.width
+    and y > object.y and y < object.y + object.height
+    then
+      object.dragging.active = true
+      object.dragging.diffX = x - object.x
+      object.dragging.diffY = y - object.y
+    end
+  end
+end
+
+
 function love:update(dt)
   dt = math.min(1/60, love.timer.getDelta())
   --print(love.timer.getDelta())
-  -- screens:update(dt)
+  --screens:update(dt)
   UPDATE_SCREENS(dt)
 end
 
