18e27fdc52b8076fc31e8a73d569f520f44d3518
Stefan Koolen <nast3zz@gmail.com>
2016-01-25 19:20:54 +0100

    Added settings screen and theme

--- screens/settings.lua
+++ screens/settings.lua
@@ -1,12 +1,25 @@
+Theme = require('assets.themes.settingsTheme')
+UI = require('lib.thranduil')
+Chatbox = require('objects.settingsScreen')
+
 settingsScreen = {}
 
 function settingsScreen:load()
+	UI.registerEvents()
+	settingsFrame = Chatbox(0, 0, 1080, 763)
 end
+settingsScreen:load()
 
-function settingsScreen:draw()
-	love.graphics.print('fsdfd')
-
+function settingsScreen:keypressed(key, gamestate)
+	if key == 'escape' then
+	    gamestate:set("menu")
+	end
 end
 
 function settingsScreen:update(dt)
+	settingsFrame:update(dt)
+end
+
+function settingsScreen:draw()
+	settingsFrame:draw()
 end
