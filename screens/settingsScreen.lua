52a4af962861ca6c56ec88decf380483ce71f3ca
rick droogh <rak_keeper@hotmail.com>
2016-02-01 16:58:36 +0100

    Menu things

--- screens/settingsScreen.lua
+++ screens/settingsScreen.lua
@@ -6,28 +6,34 @@ settingsScreen = {}
 --scale = love.window.getPixelScale( )
 
 scale = 1
+btnWidth = 112
+btnHeight = 81
 
 function settingsScreen:load()
 	settings:load()
 	UI.registerEvents()
-	musicOn = UI.Button(50, 100 * scale, 128 * scale, 128 * scale, {extensions = {settingsTheme.musicOn}, draggable = false})
-	musicOff = UI.Button(200, 100 * scale, 128 * scale, 128 * scale, {extensions = {settingsTheme.musicOff}, draggable = false})
+	musicOn = UI.Button(150, 200 * scale, btnWidth * scale, btnHeight * scale, {extensions = {settingsTheme.musicOn}, draggable = false})
+	musicOff = UI.Button(250, 200 * scale, btnWidth * scale, btnHeight * scale, {extensions = {settingsTheme.musicOff}, draggable = false})
 end
 
 function settingsScreen:draw()
-  	love.graphics.print("Settings", 500, 50, 0, 1.5, 1.5)
-	if settings:getConfigByKey('game_sound') then
-		love.graphics.rectangle("fill", 50, 100 * scale, 128 * scale, 128 * scale)
-	else
-		love.graphics.rectangle("fill", 200, 100 * scale, 128 * scale, 128 * scale)
-	end
-	musicOn:draw()
-	musicOff:draw()
+	settingsScreen:musicDraw()
 end
 
 function settingsScreen:keypressed(key, gameState)
 	if key == 'escape' then
 		gameState:set("menu")
+	end
+end
+
+function settingsScreen:musicDraw()
+	love.graphics.print("Settings", 500, 50, 0, 1.5, 1.5)
+	-- love.graphics.rectangle("fill", 200, 100 * scale, btnWidth * scale, btnHeight * scale)
+	love.graphics.setColor(255, 255, 255)
+	if settings:getConfigByKey('game_sound') then
+		musicOff:draw()
+	else
+		musicOn:draw()
 	end
 end
 
